8d7bd69ccf59cd7f78866ed8667cfc8a3141615d
exponentialR <soluadebayo@gmail.com>
2021-12-27 02:27:34 +0000

    updated seq2seq model and util_func

--- NLP/utils/util_func.py
+++ NLP/utils/util_func.py
@@ -1,4 +1,5 @@
 import tensorflow as tf #
+import tensorflow_addons as tfa
 
 def get_window_indices(sequence, target_index, half_window_size):
     left_incl = max(0, target_index - half_window_size)
@@ -13,7 +14,7 @@ def get_target_and_size(sequence, target_index, window_size):
 
 
 def get_initializer(embeddin_dim, vocab_size):
-    initial_bounds = 0.5/embedding_dim
+    initial_bounds = 0.5/embeddin_dim
     initializer = tf.random.uniform((vocab_size, embeddin_dim), minval=initial_bounds, maxval=initial_bounds)
     return initializer
 
@@ -34,3 +35,20 @@ def get_bi_state_parts(state_fw, state_bw):
     bi_state_c = tf.concat([state_fw[0], state_bw[0]], -1)
     bi_state_h = tf.concat([state_fw[1], state_bw[1]], -1)
     return bi_state_c, bi_state_h
+
+def create_basic_decoder(enc_outputs , extended_vocab_size, batch_size, final_state, dec_cell, sampler):
+    projection_layer = tf.keras.layers.Dense(extended_vocab_size)
+    batch_size = tf.constant(batch_size)
+    initial_state = dec_cell.get_initial_state(enc_outputs[0],batch_size=batch_size , dtype = tf.float32)
+    decoder = tfa.seq2seq.BasicDecoder(
+            dec_cell, sampler,
+            output_layer=projection_layer
+            )
+    return decoder
+
+def run_decoder(decoder,inputs , initial_state , input_seq_lens , isTraining ,dec_seq_lens):
+    dec_outputs, _, _ =decoder(inputs , initial_state=initial_state , sequence_length=input_seq_lens , training=isTraining) 
+    if isTraining:
+        logits = dec_outputs.rnn_output
+        return logits, dec_seq_lens
+    return dec_outputs.sample_id
